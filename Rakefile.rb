68b6c9344caf127bf34ac9cc5bca7619834ae43c
Todd Meinershagen <todd@meinershagen.net>
2017-02-25 13:16:04 -0600

    Fixing the rake build artifacts for newer version of albacore.

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,26 +1,24 @@
 #!/usr/bin/env ruby
+
 require 'albacore'
 require 'fileutils'
-CONFIG = 'Debug'
+
+CONFIG = 'Release'
 RAKE_DIR = File.expand_path(File.dirname(__FILE__))
 SOLUTION_DIR = RAKE_DIR + '/src'
 SOLUTION_FILE = 'NLog.SignalR.sln'
 NUGET = SOLUTION_DIR + "/.nuget/nuget.exe"
 
-task :default => ['build:msbuild']
-# task :test => ['build:mstest' ]
+task :default => ['build']
+# task :test => ['mstest' ]
 task :package => ['package:packall']
 task :push => ['package:pushall']
 
-namespace :build do
-
-  msbuild :msbuild, [:targets] do |msb, args|
-    args.with_defaults(:targets => :Build)
-    msb.properties :configuration => CONFIG
-    msb.targets args[:targets]
-    msb.solution = "#{SOLUTION_DIR}/#{SOLUTION_FILE}"
-  end
-  
+build :build do |b|
+  b.sln  = "#{SOLUTION_DIR}/#{SOLUTION_FILE}"
+  b.target = ['Clean', 'Rebuild']
+  b.prop 'Configuration', CONFIG
+  b.logging = 'quiet'
 end
 
 namespace :package do
@@ -33,14 +31,14 @@ namespace :package do
 		sh NUGET + " pack #{SOLUTION_DIR}/#{name}/#{name}.csproj -o pack"
 	end
 
-	task :packall => [ :clean, 'build:msbuild' ] do
+	task :packall => [ :clean, 'build' ] do
 		Dir.mkdir('pack')
 		create_packs	
 		Dir.glob('pack/*') { |file| FileUtils.move(file,'nuget/') }
 		Dir.rmdir('pack')
 	end
 
-	task :pushall => [ :clean, 'build:msbuild' ] do
+	task :pushall => [ :clean, 'build' ] do
 
 		puts "Please enter the project's NuGet API Key:"
 		key = STDIN.gets.strip
